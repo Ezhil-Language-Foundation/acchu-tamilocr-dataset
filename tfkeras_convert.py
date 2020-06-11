6400f895de544959c0288650f3067598e676c5a9
Muthiah Annamalai <ezhillang@gmail.com>
2020-06-11 11:00:17 -0700

    Models are trained at 92% on training dataset, 82% on test, and 70% on handwritten cross-validation datasets.

---
+++ tfkeras_convert.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+import os
+import numpy as np
+# Adapted from: https://keras.io/examples/mnist_cnn/
+
+def load_acchu_data(mode='train'):
+    path = os.path.split(__file__)[0]
+    labels_path = os.path.join(path,'data',mode+'-label-onehot.npy')
+    images_path = os.path.join(path,'data',mode+'-image.npy')
+    labels = np.load(labels_path)
+    images = np.load(images_path)
+    # skip the rows which are more than 2 sides exceeding boundary.
+    keep_rows = []
+    for i in range(images.shape[0]):
+        img = images[i,:].reshape(28,28)
+        hasTopFilled=any(img[0,:])
+        hasBotFilled=any(img[27,:])
+        hasLeftFilled=any(img[:,0])
+        hasRightFilled=any(img[:,27])
+        if sum([hasBotFilled, hasTopFilled, hasLeftFilled, hasRightFilled]) < 2:
+            keep_rows.append(i)
+    return labels[keep_rows,:],images[keep_rows,:]
+
+batch_size = 128
+num_classes = 13
+epochs = 200
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, split between train and test sets
+y_train, x_train  = load_acchu_data('test') #x-train.
+y_test, x_test    = load_acchu_data('train')
+
+x_train = x_train.reshape(len(x_train), img_rows, img_cols,1)
+x_test = x_test.reshape(len(x_test), img_rows, img_cols,1)
+input_shape = (img_rows, img_cols,1)
+
+half = x_test.shape[0]//2
+
+x_train = np.concatenate(np.array([x_train,x_test[0:half,:]]),0)
+y_train = np.concatenate(np.array([y_train,y_test[0:half,:]]),0)
+x_test = x_test[half+1:,:]
+y_test = y_test[half+1:,:]
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255.0
+x_test /= 255.0
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+modeldir = 'acchu_conv3_model_2'
+model = keras.models.load_model(modeldir)
+model.summary()
+
+converter = tf.lite.TFLiteConverter.from_saved_model(modeldir)
+tfl_model = converter.convert()
+with open(modeldir+'.tflite','wb') as fp:
+    fp.write(tfl_model)
+
+score = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
--- tfkeras_convpredict.py
+++ tfkeras_convpredict.py
@@ -4,6 +4,7 @@ import tensorflow.keras as keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
+import tensorflow_addons as tfa
 import os
 import numpy as np
 import tamil
@@ -38,10 +39,12 @@ img_rows, img_cols = 28, 28
 # the data, split between train and test sets
 y_train, x_train  = load_acchu_data('train')
 y_test, x_test    = load_acchu_data('test')
+#y_test=y_test[:100,:]; x_test=x_test[:100,:];
 
-x_train = x_train.reshape(len(x_train), img_rows, img_cols,1)
-x_test = x_test.reshape(len(x_test), img_rows, img_cols,1)
-input_shape = (img_rows, img_cols,1)
+
+x_train = x_train.reshape(len(x_train), img_rows, img_cols)
+x_test = x_test.reshape(len(x_test), img_rows, img_cols)
+input_shape = (img_rows, img_cols)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -52,14 +55,36 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 #model = keras.models.load_model('acchu_conv_model_5')
-model = keras.models.load_model('acchu_conv3_model_2')
+#model = keras.models.load_model('acchu_conv3_model_2')
+model = keras.models.load_model('acchu_model_4')
 model.summary()
+
+if False:
+    figure = plt.figure(figsize=(8, 8))
+    sns.heatmap(con_mat_df, annot=True,cmap=plt.cm.Blues)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+y_pred = model.predict_classes(x_test)
+y_test_index = np.zeros(y_pred.shape[0])
+y_pred_index = np.zeros(y_pred.shape[0])
+for i in range(y_pred.shape[0]):
+    y_test_index[i]=np.argmax(y_test)
+    y_pred_index[i]=np.argmax(y_pred)
+print(y_test_index)
+print(y_pred_index)
+con_mat = tf.math.confusion_matrix(labels=y_test_index, predictions=y_pred_index).numpy()
+print(con_mat)
+raise Exception()
 
 expected = ['a','aa','e','ee','u','uu','eh','aeh','ai','o','oh','au','ak']
 assert len(expected) == 13
 stats = 0.0
 total = 0.0
 msg = []
+outputF1 = tfa.metrics.F1Score(num_classes=num_classes,average='macro')
 for letter_image in glob.glob('letters-hand-drawn-corrected/*.npy'):#npy/
     total += 1.0
     print("#"*32)
@@ -75,12 +100,20 @@ for letter_image in glob.glob('letters-hand-drawn-corrected/*.npy'):#npy/
     print("predicted class=>",to_tamil_letter(predicted))
     sfx = letter_image.split('_')[1].replace('.npy','')
     print(sfx)
+    actuals = np.zeros((1,13))
+    outputP = np.zeros((1,13))
+    actuals[0,expected.index(sfx)]=1.0
+    outputP[0,predicted]=1.0
+    print(actuals)
+    print(output[0])
+    outputF1.update_state(actuals.astype(np.float32), outputP.astype(np.float32))
     if predicted != expected.index(sfx):
         stats += 1.0
         msg.append("{0} misclassified as {1}".format(to_tamil_letter(expected.index(sfx)),to_tamil_letter(predicted)))
     print(output[0])
 print("Failed cases: %g = (%g/%g)"%(stats/total,stats,total))
 print("\n".join(msg))
+print('F1 Macro score is: ',outputF1.result().numpy())
 if False:
     score = model.evaluate(x_test, y_test, verbose=1)
     print('Test loss:', score[0])
